9efbcfbb3c1603ef6b9bd0f27cc9ab9cb53c2f00
James Jay <jay_james@students.columbusstate.edu>
2026-02-01 20:39:46 -0600

    Added Formating to Profile with spacing and bolding of topic points

--- student_info.py
+++ student_info.py
@@ -5,13 +5,15 @@ email = "jay_james@students.columbusstate.edu"
 home = "Greensboro, AL"
 hobby = "Playing Games and messing with Computers"
 
-print("==========================================")
+print("\n==========================================")
 print("       STUDENT INFORMATION CARD")
 print("==========================================")
+print(f"\n\033[1mStudent's Personal Details:\033[0m")
 print(f"Name: {name}")
 print(f"Email: {email}")
 print(f"Hometown: {home}")
+print(f"Hobbies: {hobby}")
+print(f"\n\033[1mStudent's Education Details:\033[0m")
 print(f"Major: {major}")
 print(f"Graduation Year: {graduation_year}")
-print(f"Hobbies: {hobby}")
-print("==========================================")
+print("\n=================================================")
