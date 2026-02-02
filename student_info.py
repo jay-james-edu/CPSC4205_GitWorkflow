8f4e62021e3754ca58b947cb5fd5f457ce0d2142
James Jay <jay_james@students.columbusstate.edu>
2026-02-01 20:29:25 -0600

    Add hometown to profile

--- student_info.py
+++ student_info.py
@@ -2,12 +2,14 @@ name = "James Jay"
 major = "Information Technology"
 graduation_year = 2026
 email = "jay_james@students.columbusstate.edu"
+home = "Greensboro, AL"
 
 print("==========================================")
 print("       STUDENT INFORMATION CARD")
 print("==========================================")
 print(f"Name: {name}")
 print(f"Email: {email}")
+print(f"Hometown: {home}")
 print(f"Major: {major}")
 print(f"Graduation Year: {graduation_year}")
 print("==========================================")
