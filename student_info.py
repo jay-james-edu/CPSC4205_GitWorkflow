ab1ff152fadc26253cced6a1c53b15e3db543975
James Jay <jay_james@students.columbusstate.edu>
2026-02-01 20:30:35 -0600

    Add hobbies to the bottom of  profile

--- student_info.py
+++ student_info.py
@@ -3,6 +3,7 @@ major = "Information Technology"
 graduation_year = 2026
 email = "jay_james@students.columbusstate.edu"
 home = "Greensboro, AL"
+hobby = "Playing Games and messing with Computers"
 
 print("==========================================")
 print("       STUDENT INFORMATION CARD")
@@ -12,4 +13,5 @@ print(f"Email: {email}")
 print(f"Hometown: {home}")
 print(f"Major: {major}")
 print(f"Graduation Year: {graduation_year}")
+print(f"Hobbies: {hobby}")
 print("==========================================")
